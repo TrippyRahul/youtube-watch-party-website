fdb9d30e8e4189d08273e21fe35f512fdf91846a
Rahul <rahulrawattrippy@gmail.com>
2023-08-16 12:19:19 +0530

    update button link

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,37 +14,37 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type="image/x-icon" href="/logo.svg" />
-        <link rel="canonical" href="https://www.youtubewatchparty.com/" />
+        <link rel="canonical" href="https://www.youtubeparty.com/" />
 
-        <meta
-          property="og:url"
-          content="https://www.youtubewatchparty.com/"
-        />
+        <meta property="og:url" content="https://www.youtubeparty.com/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Have a blast with the Youtube watch party " />
+        <meta
+          property="og:title"
+          content="Have a blast with the Youtube watch party "
+        />
         <meta
           property="og:description"
           content="Youtube Watch Party: endless videos, movies, series & more. Chat, audio/video call while streaming together. Entertainment & fun in one place!"
         />
         <meta
           property="og:image"
-          content="https://www.youtubewatchparty.com/main-logo.svg"
+          content="https://www.youtubeparty.com/main-logo.svg"
         />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="youtubewatchparty.com" />
+        <meta property="twitter:domain" content="youtubeparty.com" />
+        <meta property="twitter:url" content="https://www.youtubeparty.com/" />
         <meta
-          property="twitter:url"
-          content="https://www.youtubewatchparty.com/"
+          name="twitter:title"
+          content="Have a blast with the Youtube watch party "
         />
-        <meta name="twitter:title" content="Have a blast with the Youtube watch party " />
         <meta
           name="twitter:description"
           content="Youtube Watch Party: endless videos, movies, series & more. Chat, audio/video call while streaming together. Entertainment & fun in one place!"
         />
         <meta
           name="twitter:image"
-          content="https://www.youtubewatchparty.com/main-logo.svg"
+          content="https://www.youtubeparty.com/main-logo.svg"
         />
       </head>
       <body>
